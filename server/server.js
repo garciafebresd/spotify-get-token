3ce5b930cd5e8ff220f245bcba5f61a7a9c67ab0
Daniel Garcia <38496841+garciafebresd@users.noreply.github.com>
2020-01-30 16:19:53 -0300

    enabled cors

--- server/server.js
+++ server/server.js
@@ -12,6 +12,12 @@ const path = require('path');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://app-spotify-firebase.herokuapp.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
